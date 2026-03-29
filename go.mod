d742948741568c3ce0354172102f317479156925
Julio Rivero <jeriveromartinez@gmail.com>
2026-03-29 12:57:59 -0600

    feat: update scheduler to use cron for daily and monthly event statistics generation

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/gin-gonic/gin v1.10.0
 	github.com/golang-jwt/jwt/v5 v5.3.1
 	github.com/google/uuid v1.6.0
+	github.com/robfig/cron/v3 v3.0.0
 	golang.org/x/crypto v0.48.0
 	google.golang.org/protobuf v1.34.1
 	gorm.io/driver/mysql v1.6.0
--- scheduler/stats.go
+++ scheduler/stats.go
@@ -1,8 +1,19 @@
 package scheduler
 
-import "github.com/jeriveromartinez/sofascore-scrapper/repository"
+import (
+	"github.com/jeriveromartinez/sofascore-scrapper/repository"
+	"github.com/robfig/cron/v3"
+)
 
 func startStats() {
-	_ = repository.GenerateDailyEventStats()
-	_ = repository.GenerateMonthlyEventStats()
+	c := cron.New()
+	_, _ = c.AddFunc("1 0 * * *", func() {
+		_ = repository.GenerateDailyEventStats()
+	})
+
+	_, _ = c.AddFunc("10 0 1 * *", func() {
+		_ = repository.GenerateMonthlyEventStats()
+	})
+
+	c.Start()
 }
