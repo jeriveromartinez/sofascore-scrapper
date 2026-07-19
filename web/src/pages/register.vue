988affd9a47873a923bc4e04d9e03ef43547b239
Julio Rivero <jeriveromartinez@gmail.com>
2026-07-18 22:59:59 -0600

    perf: reduce authentication CSS payload

--- web/src/pages/register.vue
+++ web/src/pages/register.vue
@@ -5,10 +5,8 @@ import { useAuthStore } from "../store/pinia";
 import { authApiService } from "../store/services";
 import { toast } from "vue3-toastify";
 
-import "../assets/vendor/fonts/iconify-icons.css";
-import "../assets/vendor/css/core.css";
+import "../assets/css/auth-critical.css";
 import "../assets/css/demo.css";
-import "../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css";
 import "../assets/vendor/css/pages/page-auth.css";
 import "../assets/css/inputs.css";
 
@@ -187,10 +185,8 @@ onMounted(() => checkAuth());
                     class="input-group-text cursor-pointer"
                     @click="states.revealPassword = !states.revealPassword"
                   >
-                    <i
-                      class="icon-base bx"
-                      :class="states.revealPassword ? 'bx-show' : 'bx-hide'"
-                    ></i>
+                    <svg v-if="states.revealPassword" class="password-visibility-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="currentColor"><path d="M12 9a3.02 3.02 0 0 0-3 3c0 1.642 1.358 3 3 3s3-1.358 3-3s-1.359-3-3-3"/><path d="M12 5c-7.633 0-9.927 6.617-9.948 6.684L1.946 12l.105.316C2.073 12.383 4.367 19 12 19s9.927-6.617 9.948-6.684l.106-.316l-.105-.316C21.927 11.617 19.633 5 12 5m0 12c-5.351 0-7.424-3.846-7.926-5C4.578 10.842 6.652 7 12 7c5.351 0 7.424 3.846 7.926 5c-.504 1.158-2.578 5-7.926 5"/></svg>
+                    <svg v-else class="password-visibility-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="currentColor"><path d="M12 19c.946 0 1.81-.103 2.598-.281l-1.757-1.757c-.273.021-.55.038-.841.038c-5.351 0-7.424-3.846-7.926-5a8.6 8.6 0 0 1 1.508-2.297L4.184 8.305c-1.538 1.667-2.121 3.346-2.132 3.379a1 1 0 0 0 0 .633C2.073 12.383 4.367 19 12 19m0-14c-1.837 0-3.346.396-4.604.981L3.707 2.293L2.293 3.707l18 18l1.414-1.414l-3.319-3.319c2.614-1.951 3.547-4.615 3.561-4.657a1 1 0 0 0 0-.633C21.927 11.617 19.633 5 12 5m4.972 10.558l-2.28-2.28c.19-.39.308-.819.308-1.278c0-1.641-1.359-3-3-3c-.459 0-.888.118-1.277.309L8.915 7.501A9.3 9.3 0 0 1 12 7c5.351 0 7.424 3.846 7.926 5c-.302.692-1.166 2.342-2.954 3.558"/></svg>
                   </span>
                 </div>
               </div>
